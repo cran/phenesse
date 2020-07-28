9e5cf0c7a48af75d57ae1522f16436dac62f3ff7
Michael Belitz <mbelitz@ufl.edu>
2020-07-28 22:00:06 +0000

    version 0.1.2

--- inst/doc/phenesse_vignette.R
+++ inst/doc/phenesse_vignette.R
@@ -17,33 +17,41 @@ inat_examples <- inat_examples
 ## ----Speyeria_cybele_estimates------------------------------------------------
 s_cybele <- subset(inat_examples, scientific_name == "Speyeria cybele")
 
-# calculate onset, note iterations are much lower than desired for quick 
-# vignette knit. Increase interations for robust calculations
-weib_percentile(observations = s_cybele$doy, percentile = 0, iterations = 20)
-#calculate 1st percentile
-weib_percentile(observations = s_cybele$doy, percentile = 0.1, iterations = 20)
+# calculate onset
+weib_percentile(observations = s_cybele$doy, percentile = 0.01, iterations = 250) 
+# note that the Weibull distribution does not estimate true 0th and 100th percentiles. Therefore the user must choose a percentile (quantile) between 0 and 1. 
+
+#calculate 10th percentile
+weib_percentile(observations = s_cybele$doy, percentile = 0.1, iterations = 250)
+
 #calculate 50th percentile
-weib_percentile(observations = s_cybele$doy, percentile = 0.5, iterations = 20)
+weib_percentile(observations = s_cybele$doy, percentile = 0.5) 
 
 ## ----Speyeria_cybele_CIestimates----------------------------------------------
 s_cybele <- subset(inat_examples, scientific_name == "Speyeria cybele")
 
-# calculate onset - note ignoring for vignette test
-#weib_percentile_ci(observations = s_cybele$doy, iterations = 50,
-# percentile = 0.5, bootstraps = 50)
-# note warnings of extreme order points used as endpoints is due to the 
-# low number of bootsraps used. Please use higher number of bootstraps if 
-# using for analyses.
+# calculate onset, we're using very low iterations and bootstraps to knit vignette quickly. Please increase both iterations and bootstraps if using for analyses
+weib_percentile_ci(observations = s_cybele$doy, iterations = 10,
+ percentile = 0.01, bootstraps = 100)
+# note warning that extreme order statistics used as endpoints. Increase number of bootstraps to avoid this warning.
+
+
+## ----parallelize--------------------------------------------------------------
+# parallelize the above calculation using multicore parallelization and 4 cores. 
+# weib_percentile_ci(observations = s_cybele$doy, iterations = 10,
+#                    percentile = 0.01, bootstraps = 100, 
+#                   parallelize = "multicore", ncpus = 4)
+# not run because having multiple cores in running in vignette gives check_rhub warnings
 
 ## ----quantile_CIestimates-----------------------------------------------------
 r_hirta <- subset(inat_examples, scientific_name == "Rudbeckia hirta")
 
 # calculate 50% quantile and CIs
-quantile_ci(observations = r_hirta$doy, percentile = 0.5, bootstraps = 100)
+quantile_ci(observations = r_hirta$doy, percentile = 0.5)
 
 ## ----mean_CIestimates---------------------------------------------------------
 r_hirta <- subset(inat_examples, scientific_name == "Rudbeckia hirta")
 
-# calculate 50% quantile and CIs
-mean_ci(observations = r_hirta$doy, bootstraps = 100)
+# calculate mean and CIs
+mean_ci(observations = r_hirta$doy)
 
